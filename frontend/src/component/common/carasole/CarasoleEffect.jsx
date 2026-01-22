e94d9e541f5376ae44cb26318fd9ef6123b3f810
haymanot <haymanotebabu2@gmail.com>
2026-01-22 14:03:10 -0800

    Add carousel components

---
+++ frontend/src/component/common/carasole/CarasoleEffect.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+//import { imge } from "../carasole/imge/data.js"
+import "./CarasoleEffect.css"; 
+
+function CarasoleEffect() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+  return (
+    <div className="carousel-container">
+      <Slider {...settings}>
+        {imge.map((imageItemsLink, index) => (
+          <div className="slide-container" key={index}>
+            <img
+              src={imageItemsLink}
+              alt={`slide-${index}`}
+              className="carousel-image"
+            />
+            <div className="gradient-overlay"></div>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
+
+export default CarasoleEffect;
